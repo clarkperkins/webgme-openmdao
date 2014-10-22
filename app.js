651cf31d82fbaeaba8597682df8bdb251f5edcdc
Clark Perkins <r.clark.perkins@gmail.com>
2014-10-22 15:30:49 -0500

    Stole app.js from domain-tools

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-GET THIS FILE FROM webgme-domain-tools
+/*
+ * WebGME for OpenMDAO
+ *
+ * Author: Clark Perkins
+ */
+
+var config = require('./config.json'),
+    webgme = require('webgme');
+
+// updating default configuration with ours
+webGMEGlobal.setConfig(config);
+
+// standalone server uses webGMEGlobal.getConfig() if no configuration defined
+var myServer = new webgme.standaloneServer();
+myServer.start();
